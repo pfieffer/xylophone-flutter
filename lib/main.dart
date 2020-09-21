7da1a1077d4a30c74d3a09bbce5995b4c8e3edee
Ravi Garbuja Pun <cbpmgr@gmail.com>
2020-09-21 21:37:41 +0545

    feat: Implement play sounds using keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -7,10 +8,38 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.purple, 1),
+              buildKey(Colors.indigo, 2),
+              buildKey(Colors.blue, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.yellow, 5),
+              buildKey(Colors.orange, 6),
+              buildKey(Colors.red, 7),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Expanded buildKey(MaterialColor materialColor, int soundNumber) {
+    return Expanded(
+      child: FlatButton(
+        color: materialColor,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
   }
 }
